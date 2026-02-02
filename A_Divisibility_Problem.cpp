3833df16f2b9a29d286489108b75b56360385157
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-02-02 20:38:42 +0530

    800

---
+++ A_Divisibility_Problem.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+
+void solve() {
+    ll a,b;
+    cin>>a>>b;
+
+    if(a%b==0){
+        cout<<0<<endl;
+        return;
+    }
+    int k = a/b;
+    k++;
+    cout<<k*b-a<<endl;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+// Successfully Submitted
