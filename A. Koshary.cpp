e20b1a839768602041b82ada8f5fa7ce3c510d25
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-05-01 22:55:54 +0530

    easy.

---
+++ A. Koshary.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+
+void solve() {
+    int x,y;
+    cin>>x>>y;
+    if(x&1 && y&1){
+        cout<<"NO"<<endl;
+    }
+    else{
+        cout<<"YES"<<endl;
+    }
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
