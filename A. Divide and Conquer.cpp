ed905d3b999372fe6fdb271eaf1918575ca0302e
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-07-02 22:14:28 +0530

    maths

---
+++ A. Divide and Conquer.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+
+void solve() {
+    int x,y;
+    cin >>x>>y;
+    if(x%y!=0){
+        cout<<"NO"<<endl;
+    }
+    else{
+        int z = x/y;
+        if(x%z==0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+// Successfully Submitted
