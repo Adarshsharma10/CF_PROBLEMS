d6bdf073faf4844a94acdde809e3873962ac9b18
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-07-05 23:45:28 +0530

    maths

---
+++ B. Crimson Triples.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+
+void solve() {
+    int n;
+    cin >> n;
+    ll ans = 0;
+    for(int b=1;b<=n;b++){
+        ans+= 1LL * (n/b) *(n/b);
+    }
+    cout<<ans<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+// Successfully submitted
